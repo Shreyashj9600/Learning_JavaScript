a79321a29f21d2a0c6c29abca0c71b652108ad51
Shreyashj9600 <connectshreyashj96@gmail.com>
2023-11-11 14:20:52 +0530

    dom.js

---
+++ dom-main.js
@@ -0,0 +1,15 @@
+var element;
+
+//element = document.body;
+
+//element = document.links[0];
+
+//element = document.URL;
+
+//element = document.doctype;
+
+//element = document.getElementById("Email");
+
+element = document.getElementsByClassName("coral");
+
+console.log(element);
